e4a8b7d97ed9fa994797d3f587428f6b5dfcd7d3
Risal Walangadi <risal.walangadi@gmail.com>
2024-12-03 10:43:35 +0800

    disable canvas

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
+    config.resolve.alias.canvas = false;
     config.module.rules.push({
       test: /\.node/,
       use: "raw-loader",
