19b936675ba84c9d280966050b637a567d10e857
Risal Walangadi <risal.walangadi@gmail.com>
2025-05-08 05:50:24 +0800

    add border bottom to phone number when user hover it

--- app/components/ReservationSection.tsx
+++ app/components/ReservationSection.tsx
@@ -3,18 +3,22 @@ import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa'
 
 export default function ReservationSection() {
   return (
-    <section id="reservation" className="bg-eerie text-white py-12 text-center">
+    <section id="reservation" className="bg-eerie text-white py-12 text-center space-y-3">
       <h2 className="text-xl md:text-4xl font-medium">
-        Reservasi Sekarang Melalui Whatsapp <span className="flex flex-col md:flex-none font-bold text-white text-4xl md:text-7xl ml-2">
-          <Link
-            href='https://wa.me/+6285240061230?text=Halo,+saya+ingin+bertanya.'
-            target="_blank">
-            0852-4006-1230
-          </Link>
-        </span>
+        Reservasi Sekarang Melalui Whatsapp
       </h2>
+      <p className='text-sm font-extralight text-gray-300'>Klik nomor dibawah ini</p>
+      <Link
+        href="https://wa.me/+6285240061230?text=Halo,+saya+ingin+bertanya."
+        target="_blank"
+        className="mt-2 inline-block font-bold text-white text-4xl md:text-7xl after:block after:h-[4px]
+        after:bg-orange after:scale-x-0 hover:after:scale-x-100
+        after:origin-left after:transition-transform after:duration-300"
+      >
+        0852-4006-1230
+      </Link>
 
-      <p className="mt-4 md:text-2xl text-gray-300">Hubungi kami melalui media sosial</p>
+      <p className="mt-4 md:text-xl text-gray-300">Hubungi kami melalui media sosial</p>
 
       <div className="flex justify-center gap-6 mt-4 text-2xl">
         <a href="https://www.facebook.com/profile.php?id=61566458211542"
